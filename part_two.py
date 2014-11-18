a2cdc550a1716dc2804c602190dc63af386a8b46
brianchesley <chesley.brian@gmail.com>
2014-11-17 20:55:07 -0600

    remove extra files

--- part_two.py
+++
@@ -1,8 +0,0 @@
-
-def get_cpi():
-    url = "http://www.minneapolisfed.org/community_education/teacher/calc/hist1913.cfm"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'lxml')
-    
-    
-def adjust_for_price():
